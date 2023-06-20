26f97f870e6a0d6de5e995152cea619bfbc53c93
Aleksandar Karaganchev <125347235+AleksandarKaraganchev@users.noreply.github.com>
2023-06-20 09:42:27 +0300

    Updates

--- Zadacha3/Program.cs
+++ Zadacha3/Program.cs
@@ -12,39 +12,36 @@ namespace Zadacha3
         {
             Console.ForegroundColor = ConsoleColor.DarkCyan;
             //the task with for cicle
-            //Console.Write("Въведи брой ученици: ");
-            //int n = int.Parse(Console.ReadLine());
-            //int excelentGrades = 0;
-            //int poorGrades = 0;
-            //for (int i = 0; i < n; i++)
-            //{
-            //    Console.Write($"Въведете успех: ");
-            //    double grade = double.Parse(Console.ReadLine());
-            //    if (grade >= 5.50 && grade <= 6)
-            //    {
-            //        excelentGrades++;
-            //    }
-            //    if (grade < 3 && grade >= 2)
-            //    {
-            //        poorGrades++;
-            //    }
-            //}
-            //Console.ForegroundColor = ConsoleColor.DarkGreen;
-            //Console.WriteLine("-----------------------------------------------------------------------");
-            //Console.WriteLine($"Броят на учениците със слаба оценка е |{poorGrades}|");
-            //Console.WriteLine($"Броят на учениците със отлична оценка е |{excelentGrades}|");
-            //Console.WriteLine("-----------------------------------------------------------------------");
-            //Console.WriteLine();
-
-            //the task with lambda- the faster and easier way
-            Console.Write("Въведи успеха на учениците от втора група по програмиране разделени със (space): ");
-            double[] arr = Console.ReadLine().Split(' ').Select(double.Parse).ToArray();
-            int poorGrades = arr.Count(num => num < 3 && num >= 2);
-            int excelentGrades = arr.Count(num => num >= 5.50 && num <= 6);
+            Console.Write("Въведи брой ученици: ");
+            int n = int.Parse(Console.ReadLine());
+            int excelentGrades = 0;
+            int poorGrades = 0;
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($"Въведете успех: ");
+                double grade = double.Parse(Console.ReadLine());
+                if (grade >= 5.50 && grade <= 6)
+                {
+                    excelentGrades++;
+                }
+                if (grade < 3 && grade >= 2)
+                {
+                    poorGrades++;
+                }
+                if (grade < 2 || grade > 6)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("-------------------------------------------------------");
+                    Console.WriteLine("Error!!!");
+                    Console.WriteLine("Wrong input.Please enter numbers between 2 and 6!!!");
+                    Console.WriteLine("-------------------------------------------------------");
+                    return;
+                }
+            }
             Console.ForegroundColor = ConsoleColor.DarkGreen;
             Console.WriteLine("-----------------------------------------------------------------------");
-            Console.WriteLine($"Броят на учениците със слаб успех е |{poorGrades}|");
-            Console.WriteLine($"Броят на учениците със отличен успех е |{excelentGrades}|");
+            Console.WriteLine($"Броят на учениците със слаба оценка е |{poorGrades}|");
+            Console.WriteLine($"Броят на учениците със отлична оценка е |{excelentGrades}|");
             Console.WriteLine("-----------------------------------------------------------------------");
             Console.WriteLine();
         }
